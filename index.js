de187d0ebbe4c794af172ed47898f2fa76c7ec5c
scareycs <scareycs@gmail.com>
2018-02-25 15:54:59 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+function initMap() {
+
+  var longlat = {lat: -25.363, lng: 131.044};
+  var longlat2 ={lat: -25.23, lng: 130.044};
+
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 4,
+    center: longlat
+  });
+
+  var marker = new google.maps.Marker({
+    position: longlat,
+    map: map
+  });
+
+  var marker2 = new google.maps.Marker({
+    position: longlat2,
+    map:map
+  })
+  }
