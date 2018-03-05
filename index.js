b9404bcc1b3e17d6d51c2af703541bd72b63c62c
scareycs <scareycs@gmail.com>
2018-03-05 15:35:00 -0500

    Search results accomplished through zipcode

--- index.js
+++ index.js
@@ -3,35 +3,10 @@ let geocoder;
 function initMap() {
 
   geocoder = new google.maps.Geocoder();
-  let longlat = {lat: -25.363, lng: 131.044};
-
   let map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 4,
-    center: longlat
-  });
-
-  let marker = new google.maps.Marker({
-    position: longlat,
-    map: map
-  });
-
-  let request = {
-    location: longlat ,
-    radius:4047,
-    types: ['Malls']};
-
-  let service = new google.maps.places.PlacesService(map);
-
-  service.nearbySearch(request, callback);
-
-  function callback(results, status){
-     if (status == google.maps.places.PlacesServiceStatus.OK) {
-        for (var i = 0; i < results.length; i++) {
-          var place = results[i];
-          console.log(results[i]);
-        }
-      }   
-  }
+        zoom: 4,
+        center: {lat: 40.731, lng: -73.997}
+    });
 
 
   $(".zipcodeForm").submit("click", function(event){
@@ -46,10 +21,32 @@ function initMap() {
     }, function(results, status) {
       if (status == 'OK') {
         map.setCenter(results[0].geometry.location);
-        var marker = new google.maps.Marker({
-          map: map,
-          position: results[0].geometry.location
-        });
+        let lat = results[0].geometry.location.lat(); 
+        let lng = results[0].geometry.location.lng();
+        let longlat = {lat: lat, lng: lng};
+        let request = {
+          location: longlat ,
+          radius:4000,
+          type: ['museum'],
+          keyword: ['art']};
+        let service = new google.maps.places.PlacesService(map);
+
+        service.nearbySearch(request, callback);
+
+        function callback(results, status){
+           if (status == google.maps.places.PlacesServiceStatus.OK) {
+              for (var i = 0; i < results.length; i++) {
+                var place = results[i];
+                var marker = new google.maps.Marker({
+                  map: map,
+                  position: results[0].geometry.location
+                });
+                console.log(results[i]);
+              }
+            }   
+        }
+        
+
       } else {
         window.alert('Geocode was not successful for the following reason: ' + status);
       }
