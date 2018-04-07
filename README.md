4052fab1c3ee3951a9a69e4efbc4248464d19865
scareycs <scareycs@gmail.com>
2018-04-06 21:52:23 -0400

    made radius wider for search and extended map boundaries according to markers

---
+++ README.md
@@ -0,0 +1,9 @@
+Art Escape
+	This application allows you to check the location of art museums near you. You can take a look at the locations and see whether are not the art museums are currently open. You can also get the street address of each museum in your area and check out ratings for the museums to see if the trip is going to be worthwhile.
+
+Features:
+- Be able to search Art museums within 4,000 meters of given zipo code
+- Be able to view the ratings of each result
+- See the name and address of each result
+- Be able to search multiple zipcodes without reloading the application
+
--- index.js
+++ index.js
@@ -8,22 +8,22 @@ const ratingSettings = {
   readonly: true
 }
 
-function initMap() {
-  $("#map").hide();
+function initMap() { //function that prepares map before zipcode and formats it for markers after zip code
+  $("#map").hide(); 
   geocoder = new google.maps.Geocoder();
-  let map = new google.maps.Map(document.getElementById('map'), {
+  let map = new google.maps.Map(document.getElementById('map'), { //gives geocode a generic position before user zipcode input
         zoom: 13,
         center: {lat: 40.731, lng: -73.997}
     });
-  $(".zipCodeForm").submit("click", function(event){
+  $(".zipCodeForm").submit("click", function(event){ //once zipcode is submitted...
     event.preventDefault();
-    $(".intro").hide();
+    $(".intro").hide(); // initial welcome message will be hidden
     $(".appTitle").hide();
-    $(".zipHeading").removeClass("zipHeading").addClass("appTitle2");
+    $(".zipHeading").removeClass("zipHeading").addClass("appTitle2"); // title along with zipcode submission form will be reformatted
     $("#zipLabel").addClass("zipLabel").html("Art Escape");
     $("#enterLabel").html("Zip Code");
     let zipCode = $("#zipCode").val();
-    geocoder.geocode({
+    geocoder.geocode({ 
       componentRestrictions: {
         country: 'US',
         postalCode: zipCode
@@ -33,45 +33,50 @@ function initMap() {
       });
   });
 
-  function handleGeocodeSubmit(results, status){
+  function handleGeocodeSubmit(results, status){//after postal code submission to geocode..
     if (status == 'OK') {
-      map.setCenter(results[0].geometry.location);
-      let lat = results[0].geometry.location.lat(); 
+      map.setCenter(results[0].geometry.location); //map center will be rearranges
+      let lat = results[0].geometry.location.lat(); //long and lat are set
       let lng = results[0].geometry.location.lng();
       let longlat = {lat: lat, lng: lng};
-      let request = {
-        location: longlat ,
-        radius:4000,
+      let request = { 
+        location: longlat , //based on the long and lat
+        radius:10000, //within the radius of 10000 meters
         type: ['museum'],
-        keyword: ['art']};
-      let service = new google.maps.places.PlacesService(map);
-      let mapTitle = results[0].formatted_address;
-      $('.mapTitle').html(`Showing Art Museums in <span class="resultName">${mapTitle}</span>`);
-      service.nearbySearch(request, function(results, status){
+        keyword: ['art']}; //the art museum search is done  
+      let service = new google.maps.places.PlacesService(map); //google places request is called
+      let mapTitle = results[0].formatted_address; //address is used for the title above the map
+      $('.mapTitle').html(`Showing Art Museums in <span class="resultName">${mapTitle}</span>`); 
+      service.nearbySearch(request, function(results, status){//art museum search request is called
         handleSearchResults(results, status);
       }); 
     } else {
-      $("#map").hide();
-      $('.listItems').html(``);
+      $("#map").hide();//if there is an error, the map will not show
+      $('.listItems').html(``);//result items will be emptied
       $('.mapTitle').html(``);
       //if zipcode is not valid
-      $('main').html(`<div class="error">Not a valid zipcode. Be sure to enter a valid zipcode.</div>`);
+      $('main').html(`<div class="error">Not a valid zipcode. Be sure to enter a valid zipcode.</div>`);//error message for non valid zipcodes
     }
   }
 
+  /** Create infowindows, markers, and format the map */
   function handleSearchResults(results, status){
-    
-    if (status == google.maps.places.PlacesServiceStatus.OK) {// if results returned ok
-      const infoWindowsContent = [];
-      let infoWindow = new google.maps.InfoWindow();
-      $("#map").show();
-      $('.resultsTitle').html(`You have <span class="resultNum">${results.length}</span> Results`);
+
+    // if results returned ok
+    if (status == google.maps.places.PlacesServiceStatus.OK) {
+      const infoWindowsContent = []; //content array for info window is created
+      let infoWindow = new google.maps.InfoWindow(); //info window for markers is created
+      $("#map").show(); // the map will show
+
+      // The title of the results list will show
+      $('.resultsTitle').html(`You have <span class="resultNum">${results.length}</span> Results`); 
       $('.listItems').html(`<ul>`);
       results.forEach(place => {
         let name = `<div class="listName">${place.name}</div>`;
         let openingHours = (place.opening_hours && place.opening_hours.open_now !== undefined) ?
           `<div class="openNow">Open Now! </div>`: '';
-        let ratings = (place.rating && place.rating !== undefined) ? `<div class="rating" data-rate-value="${place.rating}"></div>`:'';
+        let ratings = (place.rating && place.rating !== undefined) ? 
+          `<div class="rating" data-rate-value="${place.rating}"></div>`:'';
         let content = `${name} ${ratings} ${openingHours}`;
         
         infoWindowsContent.push(content);
@@ -83,13 +88,14 @@ function initMap() {
 
     } else if (status == google.maps.places.PlacesServiceStatus.ZERO_RESULTS){//if no results in zip code area..
       $("#map").hide();
-      $('main').html(`<div class="error">There are no art museums listed in this zip code.</div>`);
+      $('main').html(`<div class="error">There are no art museums listed in this zip code.</div>`); //this error message will appear
       $('.listItems').html(``);
       $('.mapTitle').html(``);
     }
   }
 
-  function createMarkers(results, map, infoWindow, infoWindowsContent){
+  function createMarkers(results, map, infoWindow, infoWindowsContent){ //creates markers onto the map from search
+      let bounds = new google.maps.LatLngBounds();
       for (let i = 0; i < results.length; i++) {
         let place = results[i];
         let marker = new google.maps.Marker({
@@ -97,21 +103,22 @@ function initMap() {
           position: place.geometry.location,
           title: place.name
         });
-
-        google.maps.event.addListener(marker,'click', (function(marker){ 
+        var myPlace = new google.maps.LatLng(marker.position.lat(), marker.position.lng());
+        bounds.extend(myPlace);
+        google.maps.event.addListener(marker,'click', (function(marker){  // once you click a marker..
           return function() {
-            infoWindow.setContent(infoWindowsContent[i]);
+            infoWindow.setContent(infoWindowsContent[i]); //the content is set for the info window
             $(".rating").rate(ratingSettings);
-            infoWindow.open(map,marker);
+            infoWindow.open(map,marker); // the infowindow is displayed for the marker that is clicked
           }
         })(marker));
 
         displayList(place);
       }
+      map.fitBounds(bounds);
   }
 
-  function displayList(listItem)
-  {   
+  function displayList(listItem){ //displays all results from zip code search in a list
     const photoUrl = listItem.photos && listItem.photos.length > 0 && listItem.photos[0].getUrl({maxWidth: 1000});
     const imgNode = (photoUrl === undefined) ? '' : `<img src="${photoUrl}" alt="${listItem.name}">`;
     const hours = (listItem.opening_hours && listItem.opening_hours.open_now !== undefined) ?
